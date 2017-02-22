6737546fc7461d9edd3887ee57022ddf6b04b1cf
changhui92 <a0133697@u.nus.edu>
2017-02-23 01:38:40 +0800

    Create edit project

--- edit-project.php
+++ edit-project.php
@@ -15,49 +15,77 @@
   <?php
   include_once("navbar.php");
   navbar(URL_PROJECTS_VIEW);
+
+  // $project = getProject($_GET["projectId"]);
+  $project = getProject(2);
   ?>
   <div class="container">
 
     <div class="row">
-      <div class="col-sm-12 col-md-8">
+      <form action="projectdetails2.php" method="post">
+        <div class="col-sm-12 col-md-12">
+          <div class="panel panel-default">
+            <div class="panel-heading">Edit Project: <?php echo $project[1] ?>
+              <button type="button" class="btn btn-danger" style="float: right;">
+                <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+              </button>
+            </div>
+            <div class="panel-body">
 
-        <div class="panel panel-default">
-          <div class="panel-heading">Edit Project: Project Name</div>
-          <div class="panel-body">
-
-            <form>
               <div class="col-sm-12 col-md-8">
 
                 <div class="form-group">
                   <label for="inputTitle">Project Title</label>
-                  <input type="title" class="form-control" id="inputTitle" placeholder="Enter project title">
+                  <input type="text" class="form-control" id="inputTitle" name="title" placeholder="Enter a project title" value="<?php echo $project[1] ?>"/>
                 </div>
 
                 <div class="form-group">
-                  <label for="projectDescription">Description</label>
-                  <textarea class="form-control" id="projectDescription" rows="5"></textarea>
+                  <label for="inputDescription">Description</label>
+                  <textarea class="form-control" id="inputDescription" name="description" rows="8" placeholder="Enter a project description" ><?php echo $project[4] ?></textarea>
                 </div>
 
                 <div class="form-group">
-                  <label for="imageUrl">Project Image</label>
-                  <input type="file" hidden id="imageUrl">
-                </div>
-
+                  <label for="inputImageUrl">Project Image</label>
+                  <input type="file" hidden id="inputImageUrl" name="imageUrl">
+                </br>
+                <img src="<?php echo $img;?>" width=auto height=auto/>
               </div>
-            </form>
+
+            </div>
 
           </div>
         </div>
 
       </div>
-    </div>
+      <div class="col-sm-12 col-md-12">
 
+        <?php
+        getProjectRewards($project[0]);
+        ?>
+
+      </div>
+      <div class="col-sm-12 col-md-8">
+
+        <button type="submit" class="btn btn-success" name="formUpdate">Submit
+          <?php
+            updateProject($project[0],$title,$description,$imageUrl);
+            echo $project[0];
+                        echo $title;
+                                    echo $description;
+                                                echo $imageUrl;
+          ?></button>
+        <button type="submit" class="btn btn-default" name="formCancel">Cancel</button>
+
+      </div>
+    </form>
   </div>
 
+</div>
 
-  <?php
-  include("footer.php");
-  pg_close($dbconn);
-  ?>
+
+<?php
+include("footer.php");
+pg_close($dbconn);
+?>
 </body>
 </html>
--- sqls.php
+++ sqls.php
@@ -1,63 +1,86 @@
 <?php
 function countAllProjects() {
-    $query = 'SELECT count(*) FROM projects';
-    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-    $data = pg_fetch_row($result)[0];
-    pg_free_result($result);
-    return $data;
+  $query = 'SELECT count(*) FROM projects';
+  $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+  $data = pg_fetch_row($result)[0];
+  pg_free_result($result);
+  return $data;
 }
 
 function countAllOnGoingProjects() {
-    return countAllProjects();
+  return countAllProjects();
 }
 
 function getProjectNames() {
-    $query = 'SELECT p.project_id, p.title, p.description, u.username, p.blurb FROM projects p, users u WHERE p.creator = u.username';
-    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+  $query = 'SELECT p.project_id, p.title, p.description, u.username FROM projects p, users u WHERE p.creator = u.username';
+  $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
-    while($row = pg_fetch_row($result)) {
-        echo '<div class="col-md-4">';
-        echo '<img src="' .$row[4] . '" style="max-width:100%;" />';
-        echo '<div class="panel panel-default"> <div class="panel-body" style="padding: 0 20px">';
-        echo '<h3><a href="projectdetails.php?project=' . $row[0] . '">' . $row[1] . '</a></h3>';
-        echo '<div>by <a>' . $row[3] . '</a></div>';
-        echo '<br /><br />';
-        echo '<div class="small ellipsis">' . $row[2] . '</div>';
-        echo '<br /><br />';
-        echo '<div class="progress"><div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="width:' . 70 . '%"></div></div>';
-        echo '<br />';
-        echo '</div></div>';
-        echo '</div>';
-    }
-    pg_free_result($result);
+  while($row = pg_fetch_row($result)) {
+    echo '<div class="col-md-4">';
+    echo '<div class="panel panel-default"> <div class="panel-body" style="padding: 20px">';
+    echo '<h2><a href="projectdetails2.php?project=' . $row[0] . '">' . $row[1] . '</a></h2>';
+    echo '<div ><a>' . $row[3] . '</a></div>';
+    echo '<br /><br />';
+    echo '<div class="small">' . $row[2] . '</div>';
+    echo '<br /><br />';
+    echo '<div class="progress"><div class="progress-bar" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="width:' . 70 . '%"></div></div>';
+    echo '<br />';
+    echo '</div></div>';
+    echo '</div>';
+  }
+  pg_free_result($result);
 }
 
 function isValidUser($username) {
-    $query = "SELECT u.username FROM users u WHERE u.username = '$username'";
-    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-    $bool = pg_num_rows($result) == 1;
-    pg_free_result($result);
-    echo $bool;
-    return $bool;
+  $query = "SELECT u.username FROM users u WHERE u.username = '$username'";
+  $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+  $bool = pg_num_rows($result) == 1;
+  pg_free_result($result);
+  echo $bool;
+  return $bool;
 }
 
 function getFirstName($username) {
-    $query = "SELECT u.fullname FROM users u WHERE u.username = '$username'";
-    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+  $query = "SELECT u.fullname FROM users u WHERE u.username = '$username'";
+  $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
-    $data = pg_fetch_row($result)[0];
-    pg_free_result($result);
-    return $data;
+  $data = pg_fetch_row($result)[0];
+  pg_free_result($result);
+  return $data;
 }
 
 
 function getProject($projectId){
-    $query = "SELECT * FROM projects p WHERE p.project_id='$projectId'";
-    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+  $query = "SELECT * FROM projects p WHERE p.project_id='$projectId'";
+  $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
-    $data = pg_fetch_row($result);
-    pg_free_result($result);
-    return $data;
+  $data = pg_fetch_row($result);
+  pg_free_result($result);
+  return $data;
+}
+
+function getProjectRewards($projectId){
+  $query = "SELECT r.reward_id, r.title, r.pledge, r.description, r.quantity FROM rewards r, projects p WHERE r.project_id = p.project_id AND p.project_id='$projectId'";
+  $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+  while($row = pg_fetch_row($result)) {
+    echo '<div class="col-md-4">';
+    echo '<div class="panel panel-default"> <div class="panel-body" style="padding: 20px; background-color: #C6D5DB;">';
+    echo '<h2>$' . $row[2] . ' or more</h2>';
+    echo '<br />';
+    echo '<div >' . $row[1] . '</div>';
+    echo '<div class="small">' . $row[3] . '</div>';
+    echo '<br /><br />';
+    echo '</div></div>';
+    echo '</div>';
+  }
+  pg_free_result($result);
+}
+
+function updateProject($projectId, $title, $description, $blurb){
+  $query = "UPDATE projects SET title = '$title', description = '$description', blurb = '$blurb' WHERE project_id = '$projectId'";
+  $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+  pg_free_result($result);
 }
 
 ?>
