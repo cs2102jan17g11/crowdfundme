1ee98a9c883bc96c42133bda25c45f7507a6552f
kenlsm <kenlsm@u.nus.edu>
2017-02-24 17:01:50 +0800

    Update schema, clean and data for new tables

--- sqls/Schemas.sql
+++ sqls/Schemas.sql
@@ -1,4 +1,3 @@
-/*User table*/
 CREATE TABLE Users (
   email VARCHAR(255) PRIMARY KEY,
   first_name VARCHAR(50) NOT NULL,
@@ -9,7 +8,10 @@ CREATE TABLE Users (
   role VARCHAR(5) CHECK (role = 'user' OR role = 'admin')
 );
 
-/* Project Table (Projects and funding schemas below are for testing of profile only)*/
+-- INSERT INTO Users (email, first_name, last_name, password, role)
+-- VALUES ('admin@admin.com', 'Admin', 'Admin', '$2y$10$/MV3fWHlSCzfqZAHD6ky7eCINm072KknluyL0.cR6R/bqOKwcWs0G', 'admin');
+
+/* Projects and funding schemas below are for testing of profile only*/
 CREATE TABLE Projects (
   project_id SERIAL PRIMARY KEY,
   title VARCHAR(50) NOT NULL,
@@ -23,27 +25,22 @@ CREATE TABLE Projects (
   CONSTRAINT start_date_before_end_date CHECK(start_date <= end_date)
 );
 
-/*
-/*Reward Table*/
 CREATE TABLE Rewards (
-  reward_id SERIAL NOT NULL,
+  reward_id SERIAL PRIMARY KEY,
   title VARCHAR(50) NOT NULL,
   pledge INTEGER NOT NULL,
   description TEXT NOT NULL,
   quantity INTEGER NOT NULL,
-  project_id VARCHAR(50) NOT NULL,
-  PRIMARY KEY (reward_id),
-  FOREIGN KEY project_id REFERENCES Projects(project_id)
+  project_id INTEGER NOT NULL,
+  FOREIGN KEY (project_id) REFERENCES Projects(project_id)
 );
 
-/*Fundings Table*/
 CREATE TABLE Fundings (
   funding_id VARCHAR(50) PRIMARY KEY,
   funding_datetime TIMESTAMP NOT NULL,
   amount INTEGER NOT NULL,
   email VARCHAR(255) NOT NULL,
-  project_id CHAR(50) NOT NULL,
-  FOREIGN KEY reward_id REFERENCES Rewards(reward_id),
-  FOREIGN KEY email REFERENCES Users(email)
+  reward_id INTEGER NOT NULL,
+  FOREIGN KEY (reward_id) REFERENCES Rewards(reward_id),
+  FOREIGN KEY (email) REFERENCES Users(email)
 );
-*/
