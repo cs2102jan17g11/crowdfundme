5b67858ef5b50506e5fa2edb13c8bd2d529293e8
kenlsm <kenlsm@u.nus.edu>
2017-03-21 01:23:54 +0800

    Add carousel for top-picks

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
   $dbconn = pg_connect(pg_connect_string)
   or die('Could not connect: ' . pg_last_error());
   include_once("sqls.php");
+  include_once("sql_functions/index_page/carousel.php");
 
   ?>
   <title>Explore | Projects</title>
@@ -30,17 +31,63 @@
           <br /><br />
           <hr class="half-rule"/>
           <br /><br />
-          <h4>This platform has provided access to some of the greatest ideas and innovations the world has imagined.
-            To date, We have over <?php echo countAllProjects() ?> projects, click on explore to see them.</h4>
-
-
-          </div>
+          <h4>
+            This platform has provided access to some of the greatest ideas and innovations the world has imagined.
+            To date, We have over <span class="red"><?php echo countAllProjects() ?></span> projects, click on explore to see them.
+          </h4>
         </div>
       </div>
     </div>
-    <?php
-    include("footer.php");
-    pg_close($dbconn);
-    ?>
-  </body>
-  </html>
+  </div>
+
+  <div class="container">
+  <div class="col-md-12">
+    <div class="top-picks">
+      <h1>Our Top Picks</h1>
+
+      <div id="mCarousel" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+          
+          <!-- Carousel indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#mCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#mCarousel" data-slide-to="1"></li>
+            <li data-target="#mCarousel" data-slide-to="2"></li>
+          </ol>
+
+          <!-- load top trending projects into top -->
+          <?php $top = selectTopTrendingProjects(3); ?>
+          
+          <!-- Main item -->
+          <div class="item active">
+            <?php carouselHtml($top[0]); ?>
+          </div>
+
+          <div class="item"> 
+            <?php carouselHtml($top[1]); ?>
+          </div>
+
+          <div class="item"> 
+            <?php carouselHtml($top[2]); ?>
+          </div>
+
+          <!-- Carousel controls -->
+          <a class="left carousel-control" href="#mCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#mCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  <?php
+  include("footer.php");
+  pg_close($dbconn);
+  ?>
+</body>
+</html>
---
+++ sql_functions/index_page/carousel.php
@@ -0,0 +1,38 @@
+<?php
+function selectTopTrendingProjects($limit) {
+  $result = pg_query_params('
+    SELECT p.project_id, funding_percent(p.project_id) AS pp 
+    FROM projects p 
+    WHERE funding_percent(p.project_id) IS NOT NULL 
+    ORDER BY pp DESC limit $1', array($limit)) or die('Query failed: ' . pg_last_error());
+
+  $arr = pg_fetch_all($result);
+
+  $prop_project_id = function($a) { // pick prop
+    return $a['project_id'];
+  };
+  return array_map($prop_project_id, $arr);
+}
+
+// echos given project in bootstrap carousel's item format
+function carouselHtml($project_id) {
+  $result = pg_query_params("
+    SELECT p.title, p.creator, p.img_src, p.description
+    FROM projects p
+    WHERE p.project_id = $1"
+  , array($project_id)) or die('Query failed: ' . pg_last_error());
+
+  $data = pg_fetch_row($result);
+
+  echo '<div class="row">
+          <div class="col-xs-6"><img src="' . $data[2] .'" style="max-width: 100%">
+          </div>
+          <div class="col-xs-6">
+            <h3>' . $data[0] .'</h3>
+            <p>' . $data[3] . '</p>
+          </div>
+        </div>';
+  pg_free_result($result);
+}
+
+?>
