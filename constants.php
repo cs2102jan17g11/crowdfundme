e2f10b8088336a0e2d38145fd0e766aad22fc8bc
kenlsm <kenlsm@u.nus.edu>
2017-03-29 16:01:44 +0800

    Add constants for user updates

--- constants.php
+++ constants.php
@@ -5,6 +5,8 @@
 // and last two the specific page if applicable
 define("URL_PROJECTS_CREATE", "PRCR");
 define("URL_PROJECTS_VIEW", "PRVI");
+define("URL_VIEW_USERS", "VIUS");
+define("URL_CREATE_USER", "CRUS");
 
 define("URL_INDEX", "INDE");
 
